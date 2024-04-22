087add1a52cf0f9f868c5686d49d748de335217f
Akira Kawamata <autumn@piedey.co.jp>
2024-04-22 16:47:45 +0900

    fix copy batch

--- WANGF/copyCommon.cmd
+++ WANGF/copyCommon.cmd
@@ -1,12 +1,10 @@
-set src=C:\xgit\WANGF\WANGF\OnlyEmbeddedModules\bin\%debrel%\net8.0
-cd %src%
 md C:\ProgramData\autumn
 md C:\ProgramData\autumn\WANGF
 md C:\ProgramData\autumn\WANGF\modules
 set dst=C:\ProgramData\autumn\WANGF\modules
-mkshortcut waExtOtsukai.lnk %src%\waExtOtsukai.dll
-mkshortcut waFirstOtsukai.lnk %src%\waFirstOtsukai.dll
-mkshortcut waFirstRPG.lnk %src%\waFirstRPG.dll
-mkshortcut waSimpleRPGBase.lnk %src%\waSimpleRPGBase.dll
+mkshortcut waExtOtsukai.lnk waExtOtsukai\bin\%debrel%\net8.0\waExtOtsukai.dll
+mkshortcut waFirstOtsukai.lnk waFirstOtsukai\bin\%debrel%\net8.0\waFirstOtsukai.dll
+mkshortcut waFirstRPG.lnk waFirstRPG\bin\%debrel%\net8.0\waFirstRPG.dll
+mkshortcut waSimpleRPGBase.lnk waSimpleRPGBase\bub\%debrel%\net8.0\waSimpleRPGBase.dll
 move *.lnk %dst%
 pause
